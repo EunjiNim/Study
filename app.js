504fcef86a904d2d403f5aacfd58afab824c5fc2
EunjiNim <krelena@naver.com>
2023-03-06 18:56:32 +0900

    서버 추가

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const http = require('http');
+// 서버 사용을 위해 http모듈을 http라는 변수에 담는다.
+var http = require('http');
 var fs = require('fs');
-http.createServer((request, response) => {
-    var url = request.url;
-    if (request.url == '/') {
+
+// http모듈로 서버를 생성한다. 
+var app = http.createServer(function(req,res){
+  var url = req.url;
+    if(req.url == '/'){
       url = '/main.html';
     }
-    if (request.url == '/favicon.ico') {
-      return response.writeHead(404);
+    if(req.url == '/favicon.ico'){
+      return res.writeHead(404);
     }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
-  }).listen(3000);
+    res.writeHead(200);
+    res.end(fs.readFileSync(__dirname + url));
+ 
+});
 
-console.log('Server running at http://127.0.0.1:3000/');
+// listen 함수로 3000 포트에 서버를 실행한다.
+app.listen(3000, function(){
+  console.log("server is running.")
+});
